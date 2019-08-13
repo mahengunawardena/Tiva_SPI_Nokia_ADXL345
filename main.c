f0f5c842e67a7a6b16609fbf4d6bd74ceb9cb062
Mahendra Gunawardena <mahengunawardena@users.noreply.github.com>
2019-08-12 21:26:23 -0400

    Update main.c
    Initial Commit

--- main.c
+++ main.c
@@ -10,53 +10,16 @@
 #include "driverlib/sysctl.h"
 #include "driverlib/gpio.h"
 #include "driverlib/ssi.h"
-
 int main(void)
 {
-    // Board Service Package
-
-
-
-
-
-
-
-
-
-    char *pcChars = "SSI Master send data.";
-    int32_t i32Idx;
-    int32_t temp;
+    // Board Support Package
 
     // Set the clocking to run directly from the external crystal/oscillator.
-    // SysCtlClockSet(SYSCTL_SYSDIV_1 | SYSCTL_USE_PLL | SYSCTL_OSC_INT | SYSCTL_XTAL_16MHZ);
-    //
-    // Enable the SSI0 peripheral
-    //
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_SSI0);
-    //
-    // Wait for the SSI0 module to be ready.
-    //
-    while (!SysCtlPeripheralReady(SYSCTL_PERIPH_SSI0))
-    {
-    }
-    //
-    // Configure the SSI.
-    //
-    temp=SysCtlClockGet()/1000;
-    SSIConfigSetExpClk(SSI0_BASE, SysCtlClockGet(), SSI_FRF_MOTO_MODE_0,SSI_MODE_MASTER, 2000000, 8);
-    //
-    // Enable the SSI module.
-    //
-    SSIEnable(SSI0_BASE);
-    //
-    // Send some data.
-    //
-    i32Idx = 0;
-    while (pcChars[i32Idx])
-    {
-        SSIDataPut(SSI0_BASE, pcChars[i32Idx]);
-        i32Idx++;
-    }
+    // Use the PLL (400 MHz to generate 400 MHz clock
+    // Divide 400 MHz / 2 = 200 MHz
+    // Divide 200 MHz / SYSCTL_SYSDIV_4 to generate a 20MHz Clock
+    SysCtlClockSet(SYSCTL_USE_PLL | SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ | SYSCTL_SYSDIV_10);
+
 
     return 0;
 }
